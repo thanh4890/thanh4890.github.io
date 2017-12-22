4498013d66a7b89376ce43a02b82c9bda1e014a4
thanh4890 <thanh4890@gmail.com>
2017-12-22 16:18:34 +0700

    Added public folder

---
+++ public/path---pandas-and-bananas-b3ce60024787ff23dce2.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xb0f3e884bdf1],{496:function(a,t){a.exports={data:{markdownRemark:{html:'<p>Do Pandas eat bananas? Check out this short video that shows that yes! pandas do\nseem to really enjoy bananas!</p>\n<iframe width="560" height="315" src="https://www.youtube.com/embed/4SZl1r2O_bY" frameborder="0" allowfullscreen></iframe>',frontmatter:{title:"Pandas and Bananas"}}},pathContext:{slug:"/pandas-and-bananas/"}}}});
+//# sourceMappingURL=path---pandas-and-bananas-b3ce60024787ff23dce2.js.map

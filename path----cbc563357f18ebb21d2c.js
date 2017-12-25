30b8fd737060bb0f10a4fd7939c4beb11dfb726e
thanh4890 <thanh4890@gmail.com>
2017-12-25 10:11:52 +0700

    Updates

---
+++ path----cbc563357f18ebb21d2c.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{177:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Thanh Nguyen Dac",socials:[{href:"https://github.com/thanh4890",icon:"FaGithub"},{href:"https://facebook.com/thanh4890",icon:"FaFacebook"},{href:"https://twitter.com/thanh4890",icon:"FaTwitter"}]}}},layoutContext:{}}}});
+//# sourceMappingURL=path----cbc563357f18ebb21d2c.js.map

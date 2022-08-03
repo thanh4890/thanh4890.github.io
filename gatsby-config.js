3123b5a6d9051eca2428e69b93f7a64ab9756a49
Thanh Nguyen Dac <thanh.nguyen17@tiki.vn>
2022-08-03 20:58:32 +0700

    Update blog author

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   pathPrefix: `/`,
   siteMetadata: {
     siteUrl: `https://thanh4890.netlify.com`,
-    title: `Thanh Dark`,
-    author: `Thanh Dark`,
+    title: `Thanh Dac`,
+    author: `Thanh Dac`,
     description: `Thanh Nguyen Dac personal site`,
     social: {
       twitter: 'thanh4890'
